23406d105d51cf43e6c6371f6b7cb8c6bfaacae5
KimA-Mik <ananim38@gmail.com>
2024-07-05 18:36:44 +0300

    Introduce usageStatsManager

--- app/src/main/java/com/github/kima_mik/locky/LockyApplication.kt
+++ app/src/main/java/com/github/kima_mik/locky/LockyApplication.kt
@@ -1,6 +1,8 @@
 package com.github.kima_mik.locky
 
 import android.app.Application
+import android.app.usage.UsageStatsManager
+import android.content.Context
 import com.github.kima_mik.locky.presentation.di.data
 import com.github.kima_mik.locky.presentation.di.domain
 import com.github.kima_mik.locky.presentation.di.presentation
@@ -9,8 +11,12 @@ import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
 class LockyApplication : Application() {
+    lateinit var usageStatsManager: UsageStatsManager
+        private set
+
     override fun onCreate() {
         super.onCreate()
+        usageStatsManager = getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
 
         startKoin {
             androidLogger()
