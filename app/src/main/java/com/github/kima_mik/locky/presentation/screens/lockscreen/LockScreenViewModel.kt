06a8e247d14ea2fb1c7a81b0a25f6f7c2f34cebf
KimA-Mik <ananim38@gmail.com>
2024-07-09 18:15:19 +0300

    Simple LockScreenViewModel refactor

--- app/src/main/java/com/github/kima_mik/locky/presentation/screens/lockscreen/LockScreenViewModel.kt
+++ app/src/main/java/com/github/kima_mik/locky/presentation/screens/lockscreen/LockScreenViewModel.kt
@@ -3,6 +3,7 @@ package com.github.kima_mik.locky.presentation.screens.lockscreen
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.github.kima_mik.locky.domain.applicationData.useCase.GetAppDataUseCase
+import com.github.kima_mik.locky.domain.code.DEFAULT_CODE_LENGTH
 import com.github.kima_mik.locky.presentation.elements.keyboard.KeyboardEvent
 import com.github.kima_mik.locky.presentation.screens.lockscreen.event.LockScreenUserEvent
 import kotlinx.coroutines.Dispatchers
@@ -15,7 +16,7 @@ class LockScreenViewModel(
     appData: GetAppDataUseCase
 ) : ViewModel() {
     private val hideInput = MutableStateFlow(false)
-    private val symbols = MutableStateFlow(List<String?>(4) { null })
+    private val symbols = MutableStateFlow(List<String?>(DEFAULT_CODE_LENGTH) { null })
     private val flowState = MutableStateFlow(LockScreenFlow.LAUNCH)
 
     init {
@@ -39,7 +40,7 @@ class LockScreenViewModel(
         )
     }
 
-    private var cursor = -1
+    private var cursor = 0
 
     fun onEvent(event: LockScreenUserEvent) {
         when (event) {
@@ -49,23 +50,26 @@ class LockScreenViewModel(
 
     private fun onKeyboardPress(event: KeyboardEvent) {
         when (event) {
-            KeyboardEvent.Backspace -> {
-                if (cursor < 0) {
-                    return
-                }
-                updateSymbols(cursor, null)
-                cursor = (cursor - 1)
-            }
-
+            KeyboardEvent.Backspace -> deleteSymbol()
             KeyboardEvent.Enter -> {}
-            is KeyboardEvent.Number -> {
-                if (cursor == symbols.value.size - 1) {
-                    return
-                }
-                cursor += 1
-                updateSymbols(cursor, event.code.toString())
-            }
+            is KeyboardEvent.Number -> enterSymbol(event.code.toString())
         }
+    }
+
+    private fun enterSymbol(symbol: String) {
+        if (cursor == DEFAULT_CODE_LENGTH) {
+            return
+        }
+
+        updateSymbols(cursor, symbol)
+        cursor += 1
+    }
+
+    private fun deleteSymbol() {
+        cursor = (cursor - 1).coerceAtLeast(0)
+        if (symbols.value[cursor] == null) return
+
+        updateSymbols(cursor, null)
     }
 
     private fun updateSymbols(index: Int, value: String?) {
---
+++ domain/src/main/java/com/github/kima_mik/locky/domain/code/Constants.kt
@@ -0,0 +1,4 @@
+package com.github.kima_mik.locky.domain.code
+
+const val DEFAULT_CODE_LENGTH = 4
+const val MAX_CODE_LENGTH = 6
